175f16ef6d8846791c79787bfa0715728b93db7a
Jack <jack.bonner@hotmail.com>
2022-09-19 22:48:02 +0100

    Doubled Wrap Length

--- Main Tools.py
+++ Main Tools.py
@@ -97,7 +97,7 @@ mostCommonWord.place(x=5, y=350)
 outputLabel = tk.Label(root, text ="Output:", font = ("Comic Sans MS", 10, "bold"))
 outputLabel.place(x=5, y=550)
 
-outputLabel = tk.Label(root, text = "", font = ("Comic Sans MS", 10, "bold"), wraplength = 450, justify = "left")
+outputLabel = tk.Label(root, text = "", font = ("Comic Sans MS", 10, "bold"), wraplength = 900, justify = "left")
 outputLabel.place(x=5, y=580)
 
 root.mainloop()
